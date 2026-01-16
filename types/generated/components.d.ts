0c66774549db374523725268e9d49401588820c8
Priya <priyas@qburst.com>
2026-01-16 11:26:23 +0530

    Add banner component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,33 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SectionBanner extends Struct.ComponentSchema {
+  collectionName: 'components_section_banners';
+  info: {
+    displayName: 'Banner';
+  };
+  attributes: {
+    BannerButton: Schema.Attribute.Component<'share.button', true>;
+    MainTitle: Schema.Attribute.String;
+    SubTitle: Schema.Attribute.String;
+  };
+}
+
+export interface ShareButton extends Struct.ComponentSchema {
+  collectionName: 'components_share_buttons';
+  info: {
+    displayName: 'Button';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    Label: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'section.banner': SectionBanner;
+      'share.button': ShareButton;
+    }
+  }
+}
