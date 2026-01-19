2118afa334165b025c74d5634372535e8b0d32cc
Priya Sasidharan <priya.sasidharan@zafin.com>
2026-01-19 15:33:18 +0530

    added keys

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,17 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('91c395be2613d140e126a005a5b591ef749bb6df7e7818dad41faa397bcc41a0'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('05189b86472110ef4d75923e1ca4ac112210c92dd4521bd4534c26f994e16e3e'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('ec872af3fe86b7222f0a3d97c067196406dbfec4c00c8673fbd006cf531b9fbb'),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('167d678326bf936592f542ac9b0f3bb51485f1ff7e3f1d2104133d7b66586846'),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
